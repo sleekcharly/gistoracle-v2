f832dab8aebbf4b28eaa3228463e5b4fb95eed9b
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2021-11-04 21:57:37 +0100

    fixed styling with post page

--- components/post/EditPost.js
+++ components/post/EditPost.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function EditPost() {
-  return <div></div>;
+  return (
+    <div className="flex items-center">
+      <button className="text-[#933a16] px-4 border border-[#933a16] rounded-md text-xs uppercase hover:bg-[#ffdedb]">
+        Edit
+      </button>
+    </div>
+  );
 }
 
 export default EditPost;
--- components/post/PostComponent.js
+++ components/post/PostComponent.js
@@ -134,96 +134,105 @@ function PostComponent({ postId, currentUrl }) {
   return (
     <article className="bg-white mt-[-8px] lg:mt-0 px-2 md:px-[55px] py-10 rounded-t-md">
       {/* post info section */}
-      <section className="relative mb-[20px] md:flex md:items-center md:space-x-4">
-        {/* shrine name */}
-        <div className="flex items-center">
-          <EmojiObjects color="secondary" />
+      <section className="mb-[20px] md:flex md:items-center md:space-x-4">
+        <div className="flex items-center justify-between lg:space-x-12     ">
+          <div className="flex flex-col space-y-2 lg:flex-row lg:items-center lg:space-x-4">
+            {/* shrine name */}
+            <div className="flex items-center">
+              <EmojiObjects color="secondary" />
 
-          <NextLink href={`/shrine/${shrineName}`} passHref>
-            <p className="text-[#800000] font-normal underline cursor-pointer">
-              {shrineName}
-            </p>
-          </NextLink>
-        </div>
-
-        {/* post info details */}
-        <div className="flex items-center">
-          <p className="text-black text-xs">Posted by @</p>
-
-          <NextLink href={`/user/${username}`} passHref>
-            <p className="text-[#800000] font-bold cursor-pointer">
-              {username}
-            </p>
-          </NextLink>
-
-          <p className="text-xs text-[#933a16] ml-4">
-            {dayjs(createdAt).fromNow()}
-          </p>
-
-          {/* edit post button */}
-          {currentUser && username === credentials.username ? (
-            <div>
-              <EditPost post={post} />
+              <NextLink href={`/shrine/${shrineName}`} passHref>
+                <p className="text-[#800000] font-normal underline cursor-pointer">
+                  {shrineName}
+                </p>
+              </NextLink>
             </div>
-          ) : null}
 
-          {/* post delete button */}
-          {currentUser && username === credentials.username ? (
-            <div className="ml-[30px]">
-              <Tooltip
-                title="Delete post"
-                placement="top"
-                onClick={handleDelete}
-              >
-                <IconButton size="small" color="secondary">
-                  <Delete fontSize="small" />
-                </IconButton>
-              </Tooltip>
+            {/* post info details */}
+            <div className="flex items-center space-x-2">
+              <div className="flex items-center ">
+                <p className="text-black text-xs">Posted by @</p>
 
-              <Dialog
-                open={dialogOpen}
-                onClose={handleDialogClose}
-                maxWidth="sm"
-              >
-                <DialogTitle>
-                  <div className="flex items-center">
-                    <p className="flex-grow font-semibold text-black">
-                      Delete post?
-                    </p>
-                    <IconButton onClick={handleDialogClose}>
-                      <Close />
-                    </IconButton>
-                  </div>
-                </DialogTitle>
-
-                <DialogContent dividers>
-                  <p className="text-black">
-                    This action can't be undone. This post would be removed from
-                    your profile and any shrine associated with it. Are you sure
-                    you want to do this?
+                <NextLink href={`/user/${username}`} passHref>
+                  <p className="text-[#800000] font-bold cursor-pointer">
+                    {username}
                   </p>
-                </DialogContent>
+                </NextLink>
+              </div>
 
-                <DialogActions>
-                  <div className="flex space-x-4">
-                    <button
-                      className="border border-[#933a16] border-opacity-80 text-black px-4 py-2 w-auto rounded-md hover:bg-[#933a16] hover:text-white"
-                      onClick={handleDialogClose}
-                    >
-                      Cancel
-                    </button>
-
-                    <button
-                      className="bg-[#933a16] text-white px-4 py-2 w-auto rounded-md hover:bg-[#800000]"
-                      //   onClick={handleSubmit}
-                    >
-                      Delete
-                    </button>
-                  </div>
-                </DialogActions>
-              </Dialog>
+              <p className="text-xs text-[#933a16]">
+                {dayjs(createdAt).fromNow()}
+              </p>
             </div>
-          ) : null}
+          </div>
+
+          {/* post info action buttons */}
+          <div className="flex items-center space-x-2">
+            {/* edit post button */}
+            {currentUser && username === credentials.username ? (
+              <div className="flex items-center">
+                <EditPost post={post} />
+              </div>
+            ) : null}
+
+            {/* post delete button */}
+            {currentUser && username === credentials.username ? (
+              <div className="flex items-center">
+                <Tooltip
+                  title="Delete post"
+                  placement="top"
+                  onClick={handleDelete}
+                >
+                  <IconButton size="small" color="secondary">
+                    <Delete fontSize="small" />
+                  </IconButton>
+                </Tooltip>
+
+                <Dialog
+                  open={dialogOpen}
+                  onClose={handleDialogClose}
+                  maxWidth="sm"
+                >
+                  <DialogTitle>
+                    <div className="flex items-center">
+                      <p className="flex-grow font-semibold text-black">
+                        Delete post?
+                      </p>
+                      <IconButton onClick={handleDialogClose}>
+                        <Close />
+                      </IconButton>
+                    </div>
+                  </DialogTitle>
+
+                  <DialogContent dividers>
+                    <p className="text-black">
+                      This action can't be undone. This post would be removed
+                      from your profile and any shrine associated with it. Are
+                      you sure you want to do this?
+                    </p>
+                  </DialogContent>
+
+                  <DialogActions>
+                    <div className="flex space-x-4">
+                      <button
+                        className="border border-[#933a16] border-opacity-80 text-black px-4 py-2 w-auto rounded-md hover:bg-[#933a16] hover:text-white"
+                        onClick={handleDialogClose}
+                      >
+                        Cancel
+                      </button>
+
+                      <button
+                        className="bg-[#933a16] text-white px-4 py-2 w-auto rounded-md hover:bg-[#800000]"
+                        //   onClick={handleSubmit}
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </DialogActions>
+                </Dialog>
+              </div>
+            ) : null}
+          </div>
         </div>
       </section>
 
