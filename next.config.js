8a283a5a933190d6a70756e7a426fadbcab57645
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2023-01-12 01:19:11 +0100

    setup packages and dev dependencies

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["firebasestorage.googleapis.com", "www.myparla.com", "ocdn.eu"],
+  },
 }
 
 module.exports = nextConfig
